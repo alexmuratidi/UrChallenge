9f2bc39d20578fd8c7d9c8c9e22f532e26f5faaa
alexmuratidi <alexmuratidi@mail.ru>
2018-01-20 12:07:36 +0300

    +dialogwindow

---
+++ app/src/main/java/com/hotstavropol/urchallenge1/Link.java
@@ -0,0 +1,26 @@
+package com.hotstavropol.urchallenge1;
+
+import android.util.Log;
+
+import java.util.Map;
+
+import retrofit2.Call;
+import retrofit2.http.FieldMap;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Url;
+
+/**
+ * Created by bkb on 18.01.2018.
+ */
+
+public interface Link {
+
+    @FormUrlEncoded
+    @POST("http://draglit.hol.es")
+    Call<String> post(@FieldMap Map<String, String> map);
+    
+    @GET("type=getall")
+    Call<String> get();
+}
